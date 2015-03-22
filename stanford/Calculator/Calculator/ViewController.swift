af76465b2b1829db014709d750ce9f8ba57dab8b
hqlgree2 <hqlgree2@gmail.com>
2015-03-22 18:01:23 +0800

    add autolayout and impl calculator

--- stanford/Calculator/Calculator/ViewController.swift
+++ stanford/Calculator/Calculator/ViewController.swift
@@ -23,6 +23,54 @@ class ViewController: UIViewController {
             isTyping = true
         }
     }
-
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isTyping{
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation:(Double, Double) -> Double) {
+        if 2 <= operandStack.count{
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation:(Double) -> Double) {
+        if 1 <= operandStack.count{
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = [Double]()
+    
+    @IBAction func enter() {
+        isTyping = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            isTyping = false
+        }
+    }
+    
 }
 
